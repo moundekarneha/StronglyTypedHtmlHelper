d0273064c7d7c97d6ce58f60706a054b025a0e15
HP <HP@DESKTOP-G4J7E8F>
2022-08-26 16:13:12 +0530

    Add project files.

---
+++ StronglyTypedHtmlHelper/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StronglyTypedHtmlHelper.Models;
+
+namespace StronglyTypedHtmlHelper.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                Name = "Neha",
+                Address = "Nagpur",
+                isEmployee = true
+
+            };
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+           return View();    
+        }
+    }
+}
---
+++ StronglyTypedHtmlHelper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StronglyTypedHtmlHelper.MvcApplication" Language="C#" %>
